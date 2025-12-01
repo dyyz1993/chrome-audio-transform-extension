1864310eabd3bb06f5f43da4c7dad2bf9e38a9fd
dyyz1993 <dyyz1993@qq.com>
2025-12-02 02:43:22 +0800

    Add Vite configuration for Chrome extension build

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@contracts': '/src/contracts',
+      '@adapters': '/src/adapters',
+      '@content': '/src/content',
+      '@core': '/src/core',
+      '@types': '/src/types',
+      '@popup': '/src/popup',
+      '@options': '/src/options'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    target: 'es2015',
+    rollupOptions: {
+      input: {
+        popup: 'src/popup/index.html',
+        options: 'src/options/index.html',
+        'background/index': 'src/background/index.ts',
+        'content/index': 'src/content/index.ts',
+        'content/inject': 'src/content/inject.ts',
+        // 'offscreen/index': 'src/offscreen/index.html'
+      },
+      output: {
+        entryFileNames: assetInfo => {
+          const name = assetInfo.name?.replace(/\.[jt]sx?$/, '') || 'index'
+          return `assets/${name}.js`
+        },
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  },
+  server: {
+    port: 5173
+  }
+})
